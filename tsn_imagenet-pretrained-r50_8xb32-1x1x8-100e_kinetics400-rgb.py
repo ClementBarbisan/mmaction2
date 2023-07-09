fc240383c083d87a27b8186a423d2f99f3b91938
Clément Barbisan <c.barbisan@free.fr>
2023-07-09 17:44:57 +0200

    update tests

--- tsn_imagenet-pretrained-r50_8xb32-1x1x8-100e_kinetics400-rgb.py
+++ tsn_imagenet-pretrained-r50_8xb32-1x1x8-100e_kinetics400-rgb.py
@@ -73,9 +73,9 @@ train_pipeline = [
         type='MultiScaleCrop',
         input_size=224,
         scales=(1, 0.875, 0.75, 0.66),
-        random_crop=False,
+        random_crop=True,
         max_wh_scale_gap=1),
-    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Resize', scale=(224, 224), keep_ratio=True),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
@@ -109,7 +109,7 @@ test_pipeline = [
     dict(type='PackActionInputs')
 ]
 train_dataloader = dict(
-    batch_size=16,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -128,15 +128,15 @@ train_dataloader = dict(
                 type='MultiScaleCrop',
                 input_size=224,
                 scales=(1, 0.875, 0.75, 0.66),
-                random_crop=False,
+                random_crop=True,
                 max_wh_scale_gap=1),
-            dict(type='Resize', scale=(224, 224), keep_ratio=False),
+            dict(type='Resize', scale=(224, 224), keep_ratio=True),
             dict(type='Flip', flip_ratio=0.5),
             dict(type='FormatShape', input_format='NCHW'),
             dict(type='PackActionInputs')
         ]))
 val_dataloader = dict(
-    batch_size=16,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -160,7 +160,7 @@ val_dataloader = dict(
         ],
         test_mode=True))
 test_dataloader = dict(
-    batch_size=1,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
